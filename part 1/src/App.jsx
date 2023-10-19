f5daa9fd5fa5a0c4d5644cde5c0a0cd216d28c6c
Ramgopal-Patel <124440138+Ramgopal-Patel@users.noreply.github.com>
2023-10-19 21:50:46 +0530

    Update App.jsx

--- part 1/src/App.jsx
+++ part 1/src/App.jsx
@@ -1,6 +1,6 @@
 import Button from "./Button"
 import List from "./List"
-// import Header from "./Header";
+import Header from "./Header";
 import Person from "./Person"
 
 function App() {
@@ -11,8 +11,8 @@ function App() {
       <Person name="Ramgopal Patel" age="22" />
       
       <Button text={'Click Me'} onClick={()=>alert('Hey! Ram, I am working fine.')}/>
-      {/* <Header title={'Title assignment Q3'}/> */}
-      <List items={['Apple','Banana','Papaya','Mango','Orange']}/>
+       <Header title={'Title assignment Q3'}/>
+      <List items={['Apple',' Banana', 'Papaya', 'Mango',' Orange']}/>
       
     </>
   );
